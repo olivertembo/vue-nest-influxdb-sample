da040b2ea50cd9dc0a691200882de287fad6fd80
olivertembo <tembokoliver3@gmail.com>
2023-08-06 03:46:13 -0400

    Add a data refresh button

--- web/src/views/DashboardView.vue
+++ web/src/views/DashboardView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="dashboard">
-    <div v-if="isLoading">
+    <button class="btn" @click="generateData">generate DATA</button>
+    <div class="loading" v-if="isLoading">
       Loading...
     </div>
     <div class="chart" v-if="chartLoadData && !isLoading">
@@ -29,6 +30,85 @@ import { Line } from 'vue-chartjs'
 import store from '../store';
 import { formatData } from '../utils';
 import { computed, onMounted, ref } from 'vue';
+
+const generateData = async () => {
+    if (!accessToken.value) {
+        console.error('Access token not found in store.');
+        return;
+    }
+
+    isLoading.value = true;
+
+    const headers = {
+        Authorization: `Bearer ${accessToken.value}`,
+        'Content-Type': 'application/json',
+    };
+
+    try {
+        const response = await fetch('http://localhost:4000/api/data/seed', {
+            method: 'GET', // I'm assuming POST, modify if needed.
+            headers: headers,
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to generate data via the API.');
+        }
+
+        const result = await response.json();
+        console.log('Data generated:', result);
+
+        // If you need to refresh the data in the charts after generating, 
+        // you might want to call the fetching logic again here.
+        
+    } catch (error) {
+        console.error('Error generating data:', error);
+    } finally {
+        isLoading.value = false;
+    }
+
+    await fetchData();
+};
+
+const fetchData = async () => {
+    if (!accessToken.value) {
+        throw new Error('Access token not found in store.');
+    }
+
+    const headers = {
+        Authorization: `Bearer ${accessToken.value}`,
+        'Content-Type': 'application/json',
+    };
+
+    let response = await fetch('http://localhost:4000/api/data/solar', {
+        method: 'GET',
+        headers: headers,
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch data from the API.');
+    }
+    let data = await response.json();
+
+    if (data.length > 0) {
+        solarData.value = formatData(data);
+    }
+
+    response = await fetch('http://localhost:4000/api/data/load', {
+        method: 'GET',
+        headers: headers,
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch data from the API.');
+    }
+
+    data = await response.json();
+
+    if (data.length > 0) {
+        chartLoadData.value = formatData(data);
+    }
+};
+
 
 interface DataSet {
   label: string;
@@ -69,59 +149,35 @@ const chartOptions = {
 
 const accessToken = computed(() => store.state.token);
 
+
 onMounted(async () => {
   try {
-
-    if (!accessToken.value) {
-    throw new Error('Access token not found in store.');
-  }
-
-  const headers = {
-    Authorization: `Bearer ${accessToken.value}`,
-    'Content-Type': 'application/json',
-  };
-
-
-    let response = await fetch('http://localhost:4000/api/data/solar', {
-      method: 'GET',
-      headers: headers,
-    });
-
-    if (!response.ok) {
-      throw new Error('Failed to fetch data from the API.');
+        isLoading.value = true;
+        await fetchData();
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    } finally {
+        isLoading.value = false;
     }
-    let data = await response.json();
-
-    if (data.length > 0) {
-      solarData.value = formatData(data);
-    }
-
-    response = await fetch('http://localhost:4000/api/data/load', {
-      method: 'GET',
-      headers: headers,
-    });
-
-    if (!response.ok) {
-      throw new Error('Failed to fetch data from the API.');
-    }
-
-    data = await response.json();
-
-    if (data.length > 0) {
-      chartLoadData.value = formatData(data);
-    }
-
-
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  } finally {
-    isLoading.value = false;
-  }
 });
 </script>
 <style>
 .dashboard {
   padding: 1rem;
+}
+.btn {
+  display: block;
+  margin: auto;
+  margin-bottom: 30px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  cursor: pointer;
+  width: 200px;
+}
+.loading {
+  text-align: center;
+  font-size: large;
 }
 
 .chart {
