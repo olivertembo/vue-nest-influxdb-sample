540c58d01626adb5a388d286c55eebf37c98c1fa
olivertembo <tembokoliver3@gmail.com>
2023-08-09 08:29:09 -0400

    comment out music preferences as sensors have on corolating data :)

--- backend/src/modules/index.ts
+++ backend/src/modules/index.ts
@@ -4,14 +4,11 @@ import { User } from 'src/entities/user.entity';
 import { UserController, UserService } from './user';
 
 import { Event } from 'src/entities/event.entity';
-import { EventController, EventService } from './event';
-import { MusicPreferenceController } from 'src/music-preference/music-preference.controller';
-import { MusicPreferenceService } from 'src/music-preference/music-preference.service';
 import { MusicArtist } from 'src/entities/music-artist.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Event, MusicArtist])],
-  controllers: [UserController, EventController, MusicPreferenceController],
-  providers: [UserService, EventService, MusicPreferenceService],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class AllModule {}
--- backend/src/music-preference/music-preference.controller.ts
+++ backend/src/music-preference/music-preference.controller.ts
@@ -1,57 +1,57 @@
-// music-preference.controller.ts
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Request,
-  Req,
-} from '@nestjs/common';
-import { MusicPreferenceService } from './music-preference.service';
+// // music-preference.controller.ts
+// import {
+//   Controller,
+//   Get,
+//   Post,
+//   Body,
+//   Param,
+//   Request,
+//   Req,
+// } from '@nestjs/common';
+// import { MusicPreferenceService } from './music-preference.service';
 
-import { MusicPreferenceDTO } from './dto/music-preference.dto';
-import { MusicArtist } from 'src/entities/music-artist.entity';
-import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { InRegistrationProgress, Public } from 'src/auth/auth.guard';
-import { User } from 'src/entities/user.entity';
+// import { MusicPreferenceDTO } from './dto/music-preference.dto';
+// import { MusicArtist } from 'src/entities/music-artist.entity';
+// import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
+// import { InRegistrationProgress, Public } from 'src/auth/auth.guard';
+// import { User } from 'src/entities/user.entity';
 
-@ApiBearerAuth('JWT')
-@ApiTags('music-preferences')
-@Controller('music_preferences')
-export class MusicPreferenceController {
-  constructor(
-    private readonly musicPreferenceService: MusicPreferenceService,
-  ) {}
+// @ApiBearerAuth('JWT')
+// @ApiTags('music-preferences')
+// @Controller('music_preferences')
+// export class MusicPreferenceController {
+//   constructor(
+//     private readonly musicPreferenceService: MusicPreferenceService,
+//   ) {}
 
-  @InRegistrationProgress()
-  @Get('artists')
-  @ApiResponse({
-    status: 200,
-    description: 'Success',
-    type: MusicArtist,
-    isArray: true,
-  })
-  async getAllArtists(): Promise<MusicArtist[]> {
-    return this.musicPreferenceService.getAllArtists();
-  }
+//   @InRegistrationProgress()
+//   @Get('artists')
+//   @ApiResponse({
+//     status: 200,
+//     description: 'Success',
+//     type: MusicArtist,
+//     isArray: true,
+//   })
+//   async getAllArtists(): Promise<MusicArtist[]> {
+//     return this.musicPreferenceService.getAllArtists();
+//   }
 
-  @InRegistrationProgress()
-  @Post()
-  @ApiResponse({
-    status: 201,
-    description: 'Success',
-    type: User,
-  })
-  async setUserPreferences(
-    @Req() request,
-    @Body() musicPreferenceDTO: MusicPreferenceDTO,
-  ): Promise<User> {
-    const userId = request.user?.id;
+//   @InRegistrationProgress()
+//   @Post()
+//   @ApiResponse({
+//     status: 201,
+//     description: 'Success',
+//     type: User,
+//   })
+//   async setUserPreferences(
+//     @Req() request,
+//     @Body() musicPreferenceDTO: MusicPreferenceDTO,
+//   ): Promise<User> {
+//     const userId = request.user?.id;
 
-    return this.musicPreferenceService.setUserPreferences(
-      userId,
-      musicPreferenceDTO,
-    );
-  }
-}
+//     return this.musicPreferenceService.setUserPreferences(
+//       userId,
+//       musicPreferenceDTO,
+//     );
+//   }
+// }
